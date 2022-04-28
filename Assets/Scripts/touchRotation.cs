c7aedf40b31c9556ea7157f14ea981b59d702cda
equintee <69682804+equintee@users.noreply.github.com>
2022-04-28 23:47:53 +0300

    Camera rotations added
    Moving camera with touch inputs

--- Assets/Scripts/touchRotation.cs
+++ Assets/Scripts/touchRotation.cs
@@ -5,21 +5,60 @@ using UnityEngine;
 public class touchRotation : MonoBehaviour
 {
     private Touch touch;
-    public float rotationSpeed = 1f;
-    
+    public float rotationSpeed = 0.1f;
+    Rigidbody rb;
+    public GameObject gameBoard;
+    public float _angle = 0.5f;
+    public float temp;
 
-    // Update is called once per frame
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
     void Update()
     {
+        temp = transform.localEulerAngles.y;
         if (Input.touches.Length > 0)
         {
             touch = Input.GetTouch(0);
 
             if (touch.phase == TouchPhase.Moved)
             {
-                var delta = touch.deltaPosition.x * rotationSpeed * Time.deltaTime;
-                this.transform.eulerAngles += new Vector3(0f,delta,0f);
+                var delta = touch.deltaPosition.x * rotationSpeed * Time.deltaTime * -1;
+                gameBoard.transform.eulerAngles += new Vector3(0f,delta,0f);
+
+                _angle += (touch.deltaPosition.y/Screen.height) * 2;
+                if(_angle >= Mathf.PI)
+                {
+                    _angle = Mathf.PI;
+                    return;
+                }
+                else if(_angle <= 0f)
+                {
+                    _angle = 0f;
+                    return;
+                }
+                
+
+                var offset = gameBoard.transform.position + new Vector3(0,Mathf.Sin(_angle), Mathf.Cos(_angle)) * 14;
+                transform.position = offset;
+
+                Rigidbody rb = GetComponent<Rigidbody>();
+                rb.constraints = RigidbodyConstraints.FreezePosition;
+
+                Vector3 directionToGameBoard = gameBoard.transform.position - transform.position;
+                transform.rotation = Quaternion.LookRotation(directionToGameBoard);
+
+                rb.constraints &= ~RigidbodyConstraints.FreezePosition;
             }
         }
+
+        
+        
+
     }
+
+    
+
 }
